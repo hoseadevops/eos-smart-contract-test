49488e216742a2c005aee208715b4ec2f286242e
hosea <starit@msn.cn>
2018-08-08 08:15:54 +0800

    pasua

--- contracts/eosio.token.pausable/eosio.token.pausable.hpp
+++ contracts/eosio.token.pausable/eosio.token.pausable.hpp
@@ -2,38 +2,83 @@
  *  @file
  *  @copyright defined in eos/LICENSE.txt
  */
-#include <../eosio.token/eosio.token.hpp>
+#pragma once
 
+#include <eosiolib/asset.hpp>
+#include <eosiolib/eosio.hpp>
 
-namespace eosio
-{
-    class pause: public token {
+#include <string>
 
-        public:
-            pause( account_name self ):token(self){}
-
-            void pausable( bool paused );
-            bool get_pause();
-
-            void transfer( account_name from,
-                           account_name to,
-                           asset        quantity,
-                           string       memo )
-            {
-                eosio_assert( is_pause == true, "This token is paused." );
-                token::transfer(from, to, quantity, memo);
-            }
-            void issue( account_name to,
-                        asset        quantity,
-                        string       memo )
-            {
-                eosio_assert( is_pause == true, "This token is paused." );
-                token::issue(to, quantity, memo);
-            }
-
-        private:
-            bool is_pause = false;
-    };
-
-
+namespace eosiosystem {
+   class system_contract;
 }
+
+namespace eosio {
+
+   using std::string;
+
+   class token : public contract {
+      public:
+         token( account_name self ):contract(self){}
+
+         void create( account_name issuer, asset maximum_supply);
+
+         void issue( account_name to, asset quantity, string memo );
+
+         void transfer( account_name from, account_name to, asset quantity, string memo );
+
+         inline asset get_supply( symbol_name sym )const;
+
+         inline asset get_balance( account_name owner, symbol_name sym )const;
+
+         void pausable( bool paused );
+
+         bool get_pause();
+
+      private:
+
+         bool is_pause = false;
+
+         struct account {
+           asset balance;
+           uint64_t primary_key()const { return balance.symbol.name(); }
+         };
+
+         struct currency_stats {
+            asset          supply;
+            asset          max_supply;
+            account_name   issuer;
+
+            uint64_t primary_key()const { return supply.symbol.name(); }
+         };
+
+         typedef eosio::multi_index<N(accounts), account> accounts;
+         typedef eosio::multi_index<N(stat), currency_stats> stats;
+
+         void sub_balance( account_name owner, asset value );
+         void add_balance( account_name owner, asset value, account_name ram_payer );
+
+      public:
+         struct transfer_args {
+            account_name  from;
+            account_name  to;
+            asset         quantity;
+            string        memo;
+         };
+   };
+
+   asset token::get_supply( symbol_name sym )const
+   {
+      stats statstable( _self, sym );
+      const auto& st = statstable.get( sym );
+      return st.supply;
+   }
+
+   asset token::get_balance( account_name owner, symbol_name sym )const
+   {
+      accounts accountstable( _self, owner );
+      const auto& ac = accountstable.get( sym );
+      return ac.balance;
+   }
+
+} /// namespace eosio
--- docker/eosio/container.sh
+++ docker/eosio/container.sh
@@ -62,4 +62,6 @@ _init_contract()
   run_cmd "sh eos.sh cli 'set contract eosio $project_docker_persistent_dir/contracts/eosio.bios -x 1000s -p eosio@active'"
   _open_un_lock_wallet
   run_cmd "sh eos.sh cli 'set contract dev.token $project_docker_persistent_dir/contracts/eosio.token -x 1000s -p dev.token@active'"
+  _open_un_lock_wallet
+  run_cmd "sh eos.sh cli 'set contract hexing $project_path/contracts/eosio.token.pausable -x 1000s -p hexing@active'"
 }
